080080f785727622a8f966805575b571174b07cb
Minwoo Lee <ermaker@gmail.com>
2016-05-02 12:50:35 +0900

    Update blink logic

--- blink.sh
+++ blink.sh
@@ -1,4 +1,11 @@
 while true
 do
-  nc -vlp 4225 -c './blink1-tool -m 100 -t 300 --blue --glimmer -q'
+  nc -vlp 4225 -c '
+  seq 3 | xargs -I{} sh -c "
+  ./blink1-tool --blue -v ;
+  sleep 0.5 ;
+  ./blink1-tool --off -v ;
+  sleep 0.5 ;
+  "
+  '
 done
